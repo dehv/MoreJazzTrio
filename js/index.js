f70c535bc20bfe051cce7535d055f5d68f848ac8
Matthias <freakywii@gmail.com>
2016-01-29 00:07:34 +0100

    Text Animation test

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+    
    $('#fullpage').fullpage(); 
+    x = 0;
+    setInterval(function() {
+        x-=1;
+        $('.home').css('background-position', x + 'px 0');
+    }, 30);
 });
