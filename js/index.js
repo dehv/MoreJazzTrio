5934d57f335e7ac329f7b33ca2e53f8b2490dad4
Matthias <freakywii@gmail.com>
2016-01-29 19:01:46 +0100

    merged to master

--- js/index.js
+++ js/index.js
@@ -1,34 +1,3 @@
 $(document).ready(function() {
-	function headerTextChange() {
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("private Anlässe"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("Festivals"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("Firmenveranstaltungen"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("Ausstellungen"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("Dinner- und Hintergrundmusik"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("Jubiläen"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("Hochzeiten"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
-			$(this).html("Konzerte"); n();
-		}).fadeIn(500);
-		
-		x = setTimeout(function() {
-			headerTextChange()
-		}, 3000);
-	};
-	headerTextChange();
+   $('#fullpage').fullpage(); 
 });
-
