e7d2b030777ebd3662c5c6549bfe6e6f3947ff01
dehv <david.brunschier@gmail.com>
2016-01-31 01:49:01 +0100

    Added variables

--- js/index.js
+++ js/index.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
+	var varDel=2500;
+	var varFade=500;
 	function headerTextChange() {
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("private Anlässe"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		}).fadeIn(varFade);
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("Festivals"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		}).fadeIn(varFade);
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("Firmenveranstaltungen"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		}).fadeIn(varFade);
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("Ausstellungen"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		}).fadeIn(varFade);
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("Dinner- und Hintergrundmusik"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		}).fadeIn(varFade);
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("Jubiläen"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		}).fadeIn(varFade);
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("Hochzeiten"); n();
-		}).fadeIn(500);
-		$("#text-fade").delay(3000).fadeOut(500).queue(function(n) {
+		}).fadeIn(varFade);
+		$("#text-fade").delay(varDel).fadeOut(varFade).queue(function(n) {
 			$(this).html("Konzerte"); n();
-		}).fadeIn(500);
-		
+		}).fadeIn(varFade);
+
 		x = setTimeout(function() {
 			headerTextChange()
-		}, 3000);
+		}, varDel);
 	};
 	headerTextChange();
 });
-
